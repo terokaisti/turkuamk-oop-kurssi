137f1e2b18d5066d7b201fc993321627c413467d
Tero Nurminen <tero.nurminen@gmail.com>
2010-11-24 05:17:45 +0200

    IntCalcMain ja CalcImpl main kotitehtävä

---
+++ src/geneeriset/CalcImpl.java
@@ -0,0 +1,23 @@
+package geneeriset;
+
+public class CalcImpl<Integer> implements Calculator {
+
+	@Override
+	public Number add(Number a, Number b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Number factorial(Number a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double sum(Pino pino) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
---
+++ src/geneeriset/IntCalcMain.java
@@ -0,0 +1,8 @@
+package geneeriset;
+
+public class IntCalcMain {
+	public static void main(String[] args) {
+		CalcImpl<Integer> c = new CalcImpl<Integer>();
+		
+	}
+}
